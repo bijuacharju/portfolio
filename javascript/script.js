b8eb81356a9336102681774ab35319492fbd11cf
bijuacharju <bijuacharju4@gmail.com>
2021-03-04 18:14:56 +0545

    footer in js

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,28 @@
+
+window.onload = function(){
+  footer();
+}
+function footer(){
+  let foot = '<div class="footer-content"><!-- This division holds the first block of footer section. -->';
+  foot += '<div id="content-one"><!-- This is a table to perfectly hold the data to be presented at footer section. -->';
+  foot += '<table><tr><th>Contact:</th></tr>';
+  foot += '<tr><td>+977 9817380695</td></tr>';
+  foot += '<tr><td>bijuacharju4@gmail.com</td></tr>';
+  foot += '<tr><td>www.bijuacharju.com.np</td></tr></table>';
+  foot += '</div> <!-- This division holds the second block of footer section. -->';
+  foot += '<div id="content-two"><!-- This is a table to perfectly hold the data to be presented at footer section. -->';
+  foot += '<table><tr><th>Useful-Links:</th></tr>';
+  foot += '<tr><td><a href="https://www.w3schools.com/html/default.asp" target="_blank">www.w3schools.com/html/default.asp</a></td></tr>';
+  foot += '<tr><td><a href="https://www.w3schools.com/css/default.asp" target="_blank">www.w3schools.com/css/default.asp</a></td></tr>';
+  foot += '<tr><td><a href="https://www.w3schools.com/js/default.asp" target="_blank">www.w3schools.com/js/default.asp</a></td></tr></table></div>';
+  foot += '<!-- This division holds the third block of footer section. --><div id="content-three"><p>Stay Connected:</p>';
+  foot += '<!-- Thid division holds the icon to directs the visitor towards the profile at social medias. --><div class="social-links">';
+  foot += '<a href="https://www.facebook.com/biju.acharju" target="_blank"><img src="./images/facebook.png" alt="Icon of facebook" height="30px"></a>';
+  foot += '<a href="https://www.linkedin.com/in/biju-acharju-4b4485182/" target="_blank"><img src="./images/linkedin.png" alt="Icon of linkedin" height="30px"></a>';
+  foot += '<a href="https://twitter.com/AcharjuBiju" target="_blank"><img src="./images/twitter.png" alt="Icon of twitter" height="30px"></a></div></div></div>';
+  foot += '<!-- This division represents to whom copyright of website belongs. --> <div class="copyright"><p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Designed by<strong><span style="color: #E6121B;"> Biju Acharju</span></strong></p></div>';
+  document.getElementById("footer-division").innerHTML = foot;
+}
 // When the user scrolls the page, function named myFunction() is executed.
 window.onscroll = function() {myFunction()};
 
@@ -119,5 +144,4 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() { 
   modal.style.display = "none";
 }
-
 
